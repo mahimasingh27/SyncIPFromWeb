002552aa3ef0ae5cbfc41136a2e493dd3a7d7208
Mahima Singh <smahima27@gmail.com>
2023-04-08 23:48:48 +0530

    Update pingdom.py

--- Handler/pingdom.py
+++ Handler/pingdom.py
@@ -3,6 +3,8 @@ import requests;
 class Pingdom:
     def __init__(self):
         self.URL="https://my.pingdom.com/probes/ipv4"
+        
+    #getting Ips from Pingdom IPs from Pingdom documentation
     def getPingdomProbeIp(self):
         pingdomResponse=requests.get(self.URL)
         temp=set(pingdomResponse.text.strip().split("\n"))
