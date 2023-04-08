ab2ba842094e9408032e5b18240bf78457d526bb
Mahima Singh <smahima27@gmail.com>
2023-04-08 23:41:52 +0530

    Create newRelicThread.py

---
+++ Handler/newRelicThread.py
@@ -0,0 +1,24 @@
+import threading
+import sys
+import boto3,json
+import securityGroupPermission, newRelic, pingdom
+
+class NewRelicThread(threading.Thread):
+   
+    def updateNewRelicIp(self):
+        session = boto3.Session()
+        newRelicObj=newRelic.NewRelic()
+        newRelicIps= newRelicObj.getIp()
+        getAndUpdateSecurityGrp(session,"newrelic_synthetic",newRelicIps)
+
+    def run(self):
+        self.ex = None    
+        try:
+            self.updateNewRelicIp()
+        except BaseException as e:
+            self.ex = e
+   
+    def join(self):
+        threading.Thread.join(self)
+        if self.ex:
+            raise self.ex
