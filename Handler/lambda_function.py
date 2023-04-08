4b0b883279bc5a60f59191062c6e7b2cf10889ff
Mahima Singh <smahima27@gmail.com>
2023-04-08 23:47:19 +0530

    Update lambda_function.py

--- Handler/lambda_function.py
+++ Handler/lambda_function.py
@@ -4,15 +4,19 @@ import threading
 
 #lamdahandler
 def main(event, context):    
+    #calling NewRelic Thread to sync new Relic Ips
+    t1 = newRelicthread.PingdomThread()  
     
-    t1 = newRelicthread.PingdomThread()   
+    #callingPingdom Thread to sync Pingdom Ips
     t2 = pingdomThread.NewRelicThread()
+    
     t1.start()
     t2.start()
     try:
         t1.join()
         t2.join()
         return ('Ips updation successfull')
-
+    
+    #throw exception so that we can configure alert incase of failure to update
     except Exception as e:
         raise e
