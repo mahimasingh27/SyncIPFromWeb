9b485f498604eb3b50b474928d6e73b8062467d7
Mahima Singh <smahima27@gmail.com>
2023-04-08 23:48:02 +0530

    Update newRelic.py

--- Handler/newRelic.py
+++ Handler/newRelic.py
@@ -2,9 +2,11 @@ import requests;
 import json;
 
 class NewRelic:
+    
     def __init__(self):
         self.URL="https://s3.amazonaws.com/nr-synthetics-assets/nat-ip-dnsname/production/ip-ranges.json"
     
+    #fetching Ips from Newrelic documentation
     def getIp(self):
         newrelicResponse=requests.get(self.URL)
         newRelicIp=set()
